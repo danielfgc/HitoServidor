3035b1ffe729690ac2c8ed36ef58a701c3923c07
danielfgc <daniel.fernandez@campusfp.es>
2022-11-17 13:37:08 +0100

    Backend finished, try application test, configure security.

--- src/main/java/com/springbootrestapi/models/auth/AuthResponse.java
+++ src/main/java/com/springbootrestapi/models/auth/AuthResponse.java
@@ -4,11 +4,16 @@ import lombok.Data;
 
 @Data
 public class AuthResponse {
+    private int id;
+    private String username;
     private String email;
     private String accessToken;
+
     public AuthResponse(){}
 
-    public AuthResponse(String email, String accessToken) {
+    public AuthResponse(int id, String username, String email, String accessToken) {
+        this.id = id;
+        this.username = username;
         this.email = email;
         this.accessToken = accessToken;
     }
