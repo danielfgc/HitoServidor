14b5f32cec9454ac220e3a9c15bc8bab58027779
danielfgc <daniel.fernandez@campusfp.es>
2023-02-09 11:58:21 +0100

    Backend finished, try application test, configure secustsrtrity.

--- src/main/java/com/springbootrestapi/security/WebSecurity.java
+++ src/main/java/com/springbootrestapi/security/WebSecurity.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Optional;
 
@@ -54,12 +56,20 @@ class WebSecurity{
             AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.headers().cacheControl();
-        http.csrf().disable();
-        http.cors();
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
